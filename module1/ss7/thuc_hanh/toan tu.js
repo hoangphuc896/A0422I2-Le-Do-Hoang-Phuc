d3855be10df66d875469aab5a39ffd6c6e9f2a8b
Hoangphuc896 <anhdaipro092@gmail.com>
2022-05-19 18:37:25 +0700

    bai tap

--- module1/ss7/thuc_hanh/toan tu.js
+++ module1/ss7/thuc_hanh/toan tu.js
@@ -1,7 +1,7 @@
 let inputWidth;
 let inputHeight;
-inputWidth = promp (" Enter the width ");
-inputHeight = promp (" Enter the height ");
+inputWidth = promp ('Enter the width');
+inputHeight = promp ('Enter the height');
 let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
 let area = width * height;
