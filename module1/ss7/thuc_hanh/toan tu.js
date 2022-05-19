18a44595d55a26aec329700de2facf7f423637f3
Hoangphuc896 <anhdaipro092@gmail.com>
2022-05-19 11:40:16 +0700

    bai tap

---
+++ module1/ss7/thuc_hanh/toan tu.js
@@ -0,0 +1,9 @@
+let inputWidth;
+let inputHeight;
+inputWidth = promp (" Enter the width ");
+inputHeight = promp (" Enter the height ");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("the area is: " + area);
+
