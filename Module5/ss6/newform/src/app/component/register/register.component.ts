984805078742b02c9748f2a6039244514e98cbc8
Hoangphuc896 <your email address>
2023-05-29 20:45:22 +0700

    bai_tap

--- Module5/ss6/newform/src/app/component/register/register.component.ts
+++ Module5/ss6/newform/src/app/component/register/register.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Register} from './register';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,22 +8,21 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registers: Register[] = [];
   rfRegister: FormGroup;
   country: string[] = ['Việt Nam', 'Mỹ', 'Nhật', 'Canada'];
   constructor() {
-  }
-
-  ngOnInit(): void {
     this.rfRegister = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
       passworld: new FormControl('', [Validators.required, Validators.minLength(6)]),
       confirmpassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
       country: new FormControl('', [Validators.required]),
-      age: new FormControl('', [ Validators.required, Validators.max(18)]),
+      age: new FormControl('', [ Validators.required, Validators.min(18)]),
       gender: new FormControl('', [Validators.required]),
-      phone: new FormControl('', [Validators.required, Validators.pattern(' /^\\+84\\d{9,10}$/')])
+      phone: new FormControl('', [Validators.required, Validators.pattern('^\\+84\\d{9}$')])
     });
+  }
+
+  ngOnInit(): void {
   }
 
 }
